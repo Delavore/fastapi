fe501b13066db30e383bf971ba9920f7f33209a7
kirill <92951959+Delavore@users.noreply.github.com>
2024-04-22 14:21:31 +0300

    adding header

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from typing import Annotated, Union
 import databases
 import psycopg2
 from dotenv import find_dotenv, load_dotenv
@@ -57,6 +58,10 @@ async def get_items():
     #cursor.close()
     #connect.close()
     return row
+
+@app.get("/time/")
+async def read_items(user_agent: Annotated[Item, Header()] = None):
+    return {"User-Agent": 5} # user_agent
 
 @app.post("/users/me")
 async def read(item: Item):
