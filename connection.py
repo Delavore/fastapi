5e3d517ab1639a6db72c259f49e85aff3b5f950b
kirill <kirillpanovitsyn@gmail.com>
2024-05-11 23:52:10 +0300

    Adding connection to redis db

--- connection.py
+++ connection.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session, as_declarative, declared_attr, mapped_column
 from sqlalchemy.engine import URL
 from dotenv import find_dotenv, load_dotenv
 import os
+import redis
 
 # env variables
 dotenv_path = find_dotenv()
@@ -10,6 +11,8 @@ load_dotenv(dotenv_path)
 User = os.getenv("USER")
 Password = os.getenv("PASSWD")
 Addr = os.getenv("ADDR")
+
+
 
 # url = "postgresql+psycopg2://" + User + ":" + Password + "@" + Addr + ":5432/delavore_db"
 url = URL.create(
@@ -23,3 +26,14 @@ url = URL.create(
 
 engine = create_engine(url, echo=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+# env variables
+Host = os.getenv("HOSTRD")
+Password = os.getenv("PASSWDRD")
+Addr = os.getenv("PORTRD")
+
+rd = redis.Redis(
+  host='redis-10914.c1.asia-northeast1-1.gce.redns.redis-cloud.com',
+  port=10914,
+  password='zAqwgkekQvES0Xigx9rCfQVHsf4en2wW'
+)
