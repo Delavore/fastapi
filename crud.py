03d0cb183c70f863c11ab80eecde0f388e5ea558
kirill <kirillpanovitsyn@gmail.com>
2024-05-12 01:02:45 +0300

    find bug

--- crud.py
+++ crud.py
@@ -26,8 +26,10 @@ def login(db: Session, item: Item):
 
         print("you are wellcome")
         token = str(uuid4())
+        print(token)
         deadline = (datetime.now() + timedelta(days=1)).timestamp() # time when token expires
         rd.set(token, deadline)
+        return "0"
         return token
         # return 0
 
