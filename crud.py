f06a421e79d482f2514048e3d8cd03a7709b589e
kirill <kirillpanovitsyn@gmail.com>
2024-05-12 00:22:24 +0300

    update methods redis

--- crud.py
+++ crud.py
@@ -1,5 +1,7 @@
 from model import *
 from sqlalchemy import create_engine, Integer, String, ForeignKey, select, insert
+from uuid import uuid4
+from datetime import datetime, timedelta
 
 '''
 def login(db: Session, username: str, password: str):
@@ -20,10 +22,14 @@ def login(db: Session, item: Item):
         res = db.execute(select(Users3).where(Users3.username == item.username).where(Users3.password == item.password)).scalars().all()
         if (len(res) == 0):
             print("incorrect login or password")
-            return -1
+            return "-1"
 
         print("you are wellcome")
-        return 0
+        token = str(uuid4())
+        deadline = (datetime.now() + timedelta(days=1)).timestamp() # time when token expires
+        rd.set(token, deadline)
+        return token
+        # return 0
 
 
 def register(db: Session, item: Item):
@@ -43,10 +49,12 @@ def getBooks(db: Session):
         print(res)
         return res.mappings().all()
 
-def insertBook(book: Book, db: Session):
-    with db.begin():
-        db.execute(insert(Books3).values(author=book.author, bookname=book.bookname, genre=book.genre)).scalars().all()
-        return 0
+def insertBook(tok: Token, book: Book, db: Session):
+    if rd.get(tok.token) != None and datetime.now().timestamp() <= float(rd.get(tok.token)):
+        with db.begin():
+            db.execute(insert(Books3).values(author=book.author, bookname=book.bookname, genre=book.genre)).scalars().all()
+            return 0
+    return -1
 
 def deleteBook(book: Book, db: Session):
     with db.begin():
